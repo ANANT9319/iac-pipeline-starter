1b4b2990d06cfb3391a82f535d959fbea98b5cf3
ANANT9319 <anant8052623758@gmail.com>
2026-09-04 22:54:44 +0530

    code fixed2

--- env/preprod/provider.tf
+++ env/preprod/provider.tf
@@ -2,11 +2,12 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "= 5.0.0"
+      version = "=5.0.0"
     }
   }
 }
 
+# Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
 }
